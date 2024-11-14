3c6658d47b9cff8b8845292463a10aa0c0dccc0b
KONSTANTINOS VASILOPOULOS <t8230014@aueb.gr>
2024-11-14 20:21:06 +0200

    Update AmExercise.java

--- AmExercise.java
+++ AmExercise.java
@@ -2,5 +2,6 @@ public class AmExercise {
     public void main(String[] args) {
         System.out.println("AM:8230011");
         System.out.println("AM:8230079"); 
+        System.out.println("AM:8230014");
     }
 }
