b50fe7d050834bbda42c86b0da18aacc8a61aa54
mantzarismarios <t8230088@aueb.gr>
2024-11-14 22:31:24 +0200

    Update AmExercise.java

--- AmExercise.java
+++ AmExercise.java
@@ -3,5 +3,6 @@ public class AmExercise {
         System.out.println("AM:8230011");
         System.out.println("AM:8230079"); 
         System.out.println("AM:8230014");
+        System.out.println("ΑΜ:8230088");
     }
 }
