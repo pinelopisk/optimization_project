0ea22f137df47681812c700c84e7d490d30d8c1b
artemis-t8230011 <t8230011@aueb.gr>
2024-11-25 23:06:01 +0200

    Update MainMethod.java

--- MainMethod.java
+++ MainMethod.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
 public class MainMethod {
     public static void main(String [] args) {
             Scanner input = new Scanner(System.in);
@@ -10,10 +11,12 @@ public class MainMethod {
             int k=t_n;
             int q_n=20;
             Random random= new Random();
-            int[][] mainmatrix= new int[(st_n)+4][(t_n)+4];
+            String[][] mainmatrix= new String[(st_n)+4][(t_n)+4];
             int[][] centroids = new int[t_n][q_n];
-            for (int i=3; i<t_n; i++ ) {
-                for (int j=3; j<q_n; j++) {
+            String[][] credentials= extractCredentials(mainmatrix, st_n);
+            int[][] answers= extractAnswers(mainmatrix, st_n, q_n);   
+            for (int i=2; i<t_n; i++ ) {
+                for (int j=2; j<q_n; j++) {
                     centroids[i][j] = random.nextInt(5) + 1;
                 }
             }
@@ -21,4 +24,22 @@ public class MainMethod {
 
             input.close();
     }
+    public static String[][] extractCredentials(String[][] matrix, int x) {
+        String[][] credentials= new String[x][4];
+        for (int i=0; i<x; i++){
+            for (int j=0; j<4; j++) {
+                credentials[i][j]=matrix[i][j];
+            }
+        }
+        return credentials;
+    }
+    public static int[][] extractAnswers(String[][] matrix, int z, int y) {
+        int[][] answers= new int[z][y];
+        for (int i=0; i<z; i++){
+            for (int j=3; j<matrix.length; j++) {
+                answers[i][j]=Integer.parseInt(matrix[i][j]);
+            }
+        }
+        return answers;
+    }  
 }
