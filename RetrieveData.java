a16fce36c3b1f7c5c463582b78e9cf7ddcf69b79
Themisaueb <t8210219@aueb.gr>
2024-12-20 17:55:40 +0200

    Update RetrieveData.java

--- RetrieveData.java
+++ RetrieveData.java
@@ -10,17 +10,15 @@ public class RetrieveData {
     public static List<Student> getStudents() {
         List<Student> students = new ArrayList<>();
 
-        String sql = "SELECT * FROM students"; // Επιλέγουμε όλα τα δεδομένα
+        String sql = "SELECT * FROM responses"; 
 
         try (Connection conn = DatabaseConnection.connect();
                 PreparedStatement stmt = conn.prepareStatement(sql);
                 ResultSet rs = stmt.executeQuery()) {
 
-            // Ανάκτηση των μεταδεδομένων για να βρούμε τις στήλες "answerX"
             ResultSetMetaData metaData = rs.getMetaData();
             List<String> answerColumns = new ArrayList<>();
 
-            // Εύρεση των στηλών που ξεκινούν με "answer"
             for (int i = 1; i <= metaData.getColumnCount(); i++) {
                 String columnName = metaData.getColumnName(i);
                 if (columnName.startsWith("answer")) {
@@ -28,18 +26,15 @@ public class RetrieveData {
                 }
             }
 
-            // Ανάκτηση δεδομένων ανά μαθητή
             while (rs.next()) {
                 String name = rs.getString("name");
                 int am = rs.getInt("am");
 
-                // Ανάκτηση των απαντήσεων δυναμικά
                 List<Integer> answers = new ArrayList<>();
                 for (String col : answerColumns) {
                     answers.add(rs.getInt(col));
                 }
 
-                // Δημιουργία αντικειμένου Student και προσθήκη στη λίστα
                 students.add(new Student(name, am, answers));
             }
 
