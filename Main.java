53fcf2ddf90b1c0db19431f29732bb5a46fd7bdc
KONSTANTINOS VASILOPOULOS <t8230014@aueb.gr>
2024-12-20 18:27:13 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public class Main {
-    public static void main(String[] args) {
-        // Ανάκτηση των δεδομένων των μαθητών
-        List<Students> students = RetrieveData.getStudents();
-
-    
