9b294f904cf3efc888929c24e9565c946b3f1209
artemis-t8230011 <t8230011@aueb.gr>
2024-12-31 12:30:55 +0200

    Update Kmeans.java

--- Kmeans.java
+++ Kmeans.java
@@ -1,226 +1,47 @@
-import java.net.Authenticator;
-import java.net.PasswordAuthentication;
+package GroupifyJava;
+import GroupifyJava.Student;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.Random;
 import javax.mail.*;
 import javax.mail.internet.*;
-
+ 
 public class MainMethod {
     public static void main(String[] args) {
+       
         Professor professor = RetrieveData.getProfessorData();
-        List<Students> mainList = RetrieveData.getStudents();
-        int rows = mainList.size();
-        int cols = mainList.get(0).size();
-        String[][] mainmatrix= new String[rows][cols];
-         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-            mainmatrix[i][j] = mainList.get(i).get(j);
-            }
-         }
-        String prof_mail;
-        int st_n;
-        int t_n;
-        int k = t_n;
-        int q_n = 20;
+        List<Student> mainList = RetrieveData.getStudents();
+ 
+       
+        String prof_mail = professor.getEmail();
+        int st_n = professor.getRegisteredStudents();
+        int t_n = professor.numberOfStudents();      
+        int k = st_n/t_n;
+        int q_n = 20;                              
+ 
         Random random = new Random();
         int[][] centroids = new int[k][q_n];
-        String[][] credentials = extractCredentials(mainmatrix, st_n);
-        int[][] answers = extractAnswers(mainmatrix, st_n, q_n);
         for (int i = 0; i < k; i++) {
             for (int j = 0; j < q_n; j++) {
                 centroids[i][j] = random.nextInt(5) + 1;
             }
         }
-        List<List<Integer>> clusters = kMeansClustering(answers, centroids, k, q_n);
-        String results = formatClusters(clusters, credentials);
-        String subject = "Clustering results";
-        String recipient = prof_mail;
-        sendEmail(recipient, subject, results);
-    }
-
-    public static String[][] extractCredentials(String[][] matrix, int x) {
-        String[][] credentials = new String[x][4];
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < 3; j++) {
-                credentials[i][j] = matrix[i][j];
-            }
-        }
-        return credentials;
-    }
-
-    public static int[][] extractAnswers(String[][] matrix, int z, int y) {
-        int[][] answers = new int[z][y];
-        for (int i = 0; i < z; i++) {
-            for (int j = 3; j < matrix.length; j++) {
-                answers[i][j] = Integer.parseInt(matrix[i][j]);
-            }
-        }
-        return answers;
-    }
-
-    public static String formatClusters(List<List<Integer>> clusters, String[][] credentials) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Clustering Results: \n");
-        for (int i = 0; i < clusters.size(); i++) {
-            sb.append("Team ").append(i + 1).append(":\n");
-            for (int studentIndex : clusters.get(i)) {
-                for (String value : credentials[studentIndex]) {
-                    sb.append(value).append("\t");
-                }
-                sb.append("\n");
-            }
-            sb.append("\n");
-        }
-
-        return sb.toString();
-    }
-
-    public static void sendEmail(String recipient, String subject, String messageBody) {
-        final String senderEmail = "groupify.9@gmail.com";
-        final String senderPassword = "123456789.groupify";
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        Session session = Session.getInstance(props, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(senderEmail, senderPassword);
-            }
-        });
-        MimeMessage message = new MimeMessage(session);
-        message.setFrom(new InternetAddress(senderEmail));
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
-        message.setSubject(subject);
-        message.setText(messageBody);
-        Transport.send(message);
-    }
-
-    public static List<List<Integer>> kMeansClustering(int[][] answers, int[][] centroids, int k, int q_n) {
-        int numStudents = answers.length;
-        boolean converged = false;
-        List<List<Integer>> clusters = new ArrayList<>();
-
-        while (!converged) {
-            clusters = new ArrayList<>();
-            for (int i = 0; i < k; i++) {
-                clusters.add(new ArrayList<>());
-            }
-
-            for (int i = 0; i < numStudents; i++) {
-                int closestCentroidIndex = 0;
-                double minDistance = calculateDistance(answers[i], centroids[0]);
-
-                for (int j = 1; j < k; j++) {
-                    double distance = calculateDistance(answers[i], centroids[j]);
-                    if (distance < minDistance) {
-                        minDistance = distance;
-                        closestCentroidIndex = j;
-                    }
-                }
-
-                clusters.get(closestCentroidIndex).add(i);
-            }
-
-            int[][] newCentroids = new int[k][q_n];
-            for (int i = 0; i < k; i++) {
-                List<Integer> cluster = clusters.get(i);
-                if (!cluster.isEmpty()) {
-                    for (int studentIndex : cluster) {
-                        for (int j = 0; j < q_n; j++) {
-                            newCentroids[i][j] += answers[studentIndex][j];
-                        }
-                    }
-                    for (int j = 0; j < q_n; j++) {
-                        newCentroids[i][j] /= cluster.size();
-                    }
-                } else {
-                    newCentroids[i] = centroids[i];
-                }
-            }
-
-            converged = true;
-            for (int i = 0; i < k; i++) {
-                if (!areEqual(centroids[i], newCentroids[i])) {
-                    converged = false;
-                    break;
-                }
-            }
-            centroids = newCentroids;
-        }
-
-        return clusters;
-    }
-
-    public static double calculateDistance(int[] student1, int[] student2) {
-        double sum = 0;
-        for (int i = 0; i < student1.length; i++) {
-            sum += Math.pow(student1[i] - student2[i], 2);
-        }
-        return Math.sqrt(sum);
-    }
-
-    public static boolean areEqual(int[] centroid1, int[] centroid2) {
-        for (int i = 0; i < centroid1.length; i++) {
-            if (centroid1[i] != centroid2[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
-
-
-//αλλαγμένη μορφή 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.Random;
-import javax.mail.*;
-import javax.mail.internet.*;
-
-public class MainMethod {
-    public static void main(String[] args) {
-        // Ανάκτηση δεδομένων από τη βάση
-        Professor professor = RetrieveData.getProfessorData();
-        List<Student> mainList = RetrieveData.getStudents();
-
-        // Εξαγωγή δεδομένων από τον καθηγητή
-        String prof_mail = professor.getEmail();
-        int st_n = professor.getRegisteredStudents(); // Αριθμός φοιτητών
-        int t_n = professor.getNumberOfTeams();      // Αριθμός ομάδων (k)
-        int k = t_n;
-        int q_n = 20;                                // Αριθμός ερωτήσεων
-
-        // Δημιουργία αρχικών κεντροειδών
-        Random random = new Random();
-        int[][] centroids = new int[k][q_n];
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < q_n; j++) {
-                centroids[i][j] = random.nextInt(5) + 1; // Απαντήσεις μεταξύ 1 και 5
-            }
-        }
-
-        // Εξαγωγή credentials και απαντήσεων
+ 
         String[][] credentials = extractCredentials(mainList);
         int[][] answers = extractAnswers(mainList, st_n, q_n);
-
-        // Εκτέλεση του αλγορίθμου K-Means
+ 
         List<List<Integer>> clusters = kMeansClustering(answers, centroids, k, q_n);
-
-        // Δημιουργία αποτελεσμάτων
+ 
+     
         String results = formatClusters(clusters, credentials);
-
-        // Αποστολή αποτελεσμάτων μέσω email
+ 
+     
         String subject = "Clustering Results";
         sendEmail(prof_mail, subject, results);
     }
-
-    // Εξαγωγή credentials (όνομα, επώνυμο, AM)
+ 
+ 
     public static String[][] extractCredentials(List<Student> students) {
         int size = students.size();
         String[][] credentials = new String[size][3];
@@ -232,8 +53,7 @@ public class MainMethod {
         }
         return credentials;
     }
-
-    // Εξαγωγή απαντήσεων
+ 
     public static int[][] extractAnswers(List<Student> students, int st_n, int q_n) {
         int[][] answers = new int[st_n][q_n];
         for (int i = 0; i < st_n; i++) {
@@ -244,8 +64,7 @@ public class MainMethod {
         }
         return answers;
     }
-
-    // Μορφοποίηση αποτελεσμάτων σε string για email
+ 
     public static String formatClusters(List<List<Integer>> clusters, String[][] credentials) {
         StringBuilder sb = new StringBuilder();
         sb.append("Clustering Results: \n");
@@ -262,8 +81,8 @@ public class MainMethod {
         }
         return sb.toString();
     }
-
-    // Μέθοδος αποστολής email
+ 
+ 
     public static void sendEmail(String recipient, String subject, String messageBody) {
         final String senderEmail = "groupify.9@gmail.com";
         final String senderPassword = "123456789.groupify";
@@ -289,24 +108,23 @@ public class MainMethod {
             e.printStackTrace();
         }
     }
-
-    // K-Means Clustering
+ 
+   
     public static List<List<Integer>> kMeansClustering(int[][] answers, int[][] centroids, int k, int q_n) {
         int numStudents = answers.length;
         boolean converged = false;
         List<List<Integer>> clusters = new ArrayList<>();
-
+ 
         while (!converged) {
             clusters = new ArrayList<>();
             for (int i = 0; i < k; i++) {
                 clusters.add(new ArrayList<>());
             }
-
-            // Ανάθεση φοιτητών στα πλησιέστερα κεντροειδή
+ 
             for (int i = 0; i < numStudents; i++) {
                 int closestCentroidIndex = 0;
                 double minDistance = calculateDistance(answers[i], centroids[0]);
-
+ 
                 for (int j = 1; j < k; j++) {
                     double distance = calculateDistance(answers[i], centroids[j]);
                     if (distance < minDistance) {
@@ -314,11 +132,10 @@ public class MainMethod {
                         closestCentroidIndex = j;
                     }
                 }
-
+ 
                 clusters.get(closestCentroidIndex).add(i);
             }
-
-            // Ενημέρωση κεντροειδών
+ 
             int[][] newCentroids = new int[k][q_n];
             for (int i = 0; i < k; i++) {
                 List<Integer> cluster = clusters.get(i);
@@ -335,8 +152,8 @@ public class MainMethod {
                     newCentroids[i] = centroids[i];
                 }
             }
-
-            // Έλεγχος σύγκλισης
+ 
+           
             converged = true;
             for (int i = 0; i < k; i++) {
                 if (!areEqual(centroids[i], newCentroids[i])) {
@@ -346,10 +163,10 @@ public class MainMethod {
             }
             centroids = newCentroids;
         }
-
+ 
         return clusters;
     }
-
+ 
     public static double calculateDistance(int[] student1, int[] student2) {
         double sum = 0;
         for (int i = 0; i < student1.length; i++) {
@@ -357,7 +174,7 @@ public class MainMethod {
         }
         return Math.sqrt(sum);
     }
-
+ 
     public static boolean areEqual(int[] centroid1, int[] centroid2) {
         for (int i = 0; i < centroid1.length; i++) {
             if (centroid1[i] != centroid2[i]) {
